9eca0c4fd743a21f723f4b274064a4e448e7adaa
Nompilon <ngwenyanompilo@gmail.com>
2025-10-02 15:20:54 +0200

    ...

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
-
+const inventoryRoute = require("./routes/inventoryRoute")
 /* ***********************
  * View Engine and Templates
  *************************/
@@ -27,6 +27,11 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.get("/", baseController.buildHome)
 app.use(static)
+
+/* ***********************
+ * Inventory routes
+ *************************/  
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
